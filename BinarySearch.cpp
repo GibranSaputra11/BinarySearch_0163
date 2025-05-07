2dd8b40585b0e68c11f19c02b7e41bb051b0eca8
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-07 14:54:11 +0700

    add algoritma bubbleSortArray

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -28,6 +28,21 @@ void input(){
     }
 }
 
+void bubbleSortArray(){
+    int pass =1; //step 1
+    do {
+        for (int j=0;j<=n-1-pass;j++){ //step 2
+            if (arr[j] > arr[j+1]){ //setep 3
+            int temp; //mendeklarasiakn varible semtara temporay
+            temp = arr[j];
+            arr [j] = arr[j+1];
+            arr [j+1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4
+    }while (pass<=n-1); //step 5
+}
+
 int main(){
     
 }
